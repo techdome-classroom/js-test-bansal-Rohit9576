30411ae71d337a7b719f8ba6304191a708739035
Rohit9576 <130234714+Rohit9576@users.noreply.github.com>
2024-02-23 07:21:24 +0000

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,19 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    def isValid(s: str) -> bool:
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
     
 };
 
