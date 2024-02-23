d33cb2086c8e46b41a1c4cbd8e4152d5db551164
Rohit9576 <130234714+Rohit9576@users.noreply.github.com>
2024-02-23 07:52:09 +0000

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,36 +3,31 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    var romanToInt = function(s) {
-        const romanNumerals = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        };
-    
-        let result = 0;
-    
-        for (let i = 0; i < s.length; i++) {
-            const currentSymbol = romanNumerals[s[i]];
-            const nextSymbol = romanNumerals[s[i + 1]];
-    
-            if (nextSymbol && currentSymbol < nextSymbol) {
-                result += nextSymbol - currentSymbol;
-                i++; // Skip the next symbol since it's already considered
-            } else {
-                result += currentSymbol;
-            }
-        }
-    
-        return result;
+    const romanNumerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     };
-    
-    
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const currentSymbol = romanNumerals[s[i]];
+        const nextSymbol = romanNumerals[s[i + 1]];
+
+        if (nextSymbol && currentSymbol < nextSymbol) {
+            result += nextSymbol - currentSymbol;
+            i++; // Skip the next symbol since it's already considered
+        } else {
+            result += currentSymbol;
+        }
+    }
+
+    return result;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
